50bfe01e4bee0ad44699a15aa28842f600169d36
boogiemonsta <boogiemonsta@gmail.com>
2022-05-28 17:58:31 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<MainWrapper className='App'>
+			<EditorWrapper>
+				<EditorTitle>Type your Markdown here</EditorTitle>
+				<TextArea name='editor' id='editor' cols='30' rows='10' />
+			</EditorWrapper>
+			<PreviewWrapper>
+				<PreviewTitle>Preview</PreviewTitle>
+				<div id='preview'></div>
+			</PreviewWrapper>
+		</MainWrapper>
+	);
 }
 
 export default App;
+
+const EditorWrapper = styled.div`
+	background-color: black;
+	width: 50vw;
+`;
+
+const EditorTitle = styled.h2`
+	color: white;
+`;
+
+const TextArea = styled.textarea`
+	border: none;
+`;
+
+const PreviewWrapper = styled.div`
+	background-color: white;
+  width: 50vw;
+`;
+
+const PreviewTitle = styled.h2`
+	color: black;
+`;
+
+const MainWrapper = styled.div`
+	height: 100vh;
+	width: 100vw;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+`;
