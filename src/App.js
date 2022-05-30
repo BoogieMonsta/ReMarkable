f11afcf7e510649aebce441325605f8d4f10bd94
boogiemonsta <boogiemonsta@gmail.com>
2022-05-30 12:29:45 +0200

    marked.js, dompurify

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import { marked } from 'marked';
+import DOMPurify from 'dompurify';
 import './App.css';
 
 function App() {
+	const example = `# This is a h1 header
+
+## This is a h2 header
+  
+This is [a link](http://url.com/)
+
+\`<p>This is inline code</p>\`
+
+\`\`\`
+<p>This is a code block</p>
+\`\`\`
+
+* this is a list item
+* and another one
+
+> This is a blockquote
+
+This is an image
+
+![cute-animal](https://media.wired.com/photos/593261cab8eb31692072f129/master/w_320,c_limit/85120553.jpg)
+
+**This is bolded text**`
+
+	const exampleTranslated = DOMPurify.sanitize(marked.parse(example));
+
+	const [markdown, setMarkdown] = useState(exampleTranslated);
+  marked.setOptions({
+    breaks: true
+  });
+	const handleChange = e => {
+		setMarkdown(DOMPurify.sanitize(marked.parse(e.target.value)));
+	};
 	return (
 		<MainWrapper className='App'>
 			<EditorWrapper>
 				<EditorTitle>Type your Markdown here</EditorTitle>
-				<TextArea name='editor' id='editor' cols='30' rows='10' />
+				<TextArea
+					spellCheck='false'
+					name='editor'
+					id='editor'
+					onChange={handleChange}
+          defaultValue={example}
+				/>
 			</EditorWrapper>
 			<PreviewWrapper>
-				<PreviewTitle>Preview</PreviewTitle>
-				<div id='preview'></div>
+				<div id='preview' dangerouslySetInnerHTML={{__html: markdown}}/>
 			</PreviewWrapper>
 		</MainWrapper>
 	);
@@ -20,7 +60,10 @@ export default App;
 
 const EditorWrapper = styled.div`
 	background-color: gray;
-	width: 50vw;
+	width: 60vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const EditorTitle = styled.h2`
@@ -29,7 +72,7 @@ const EditorTitle = styled.h2`
 
 const TextArea = styled.textarea`
 	border: none;
-	width: 44vw;
+	width: 85%;
 	height: 86vh;
 	resize: none;
 	background: none;
@@ -42,10 +85,8 @@ const TextArea = styled.textarea`
 const PreviewWrapper = styled.div`
 	background-color: white;
 	width: 50vw;
-`;
-
-const PreviewTitle = styled.h2`
-	color: darkgray;
+	padding: 75px;
+	text-align: left;
 `;
 
 const MainWrapper = styled.div`
